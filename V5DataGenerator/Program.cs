74aa0dfd241dfecf2f14ece3de86a8cc70dbd05e
Korolev Dmitry <deagle.gross@gmail.com>
2021-02-24 12:17:29 +0300

    fix paths to V5DataGenerator

---
+++ V5DataGenerator/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+namespace V5DataGenerator
+{
+    public class Program
+    {
+        private static readonly Random Random = new Random();
+
+        static float NextFloat()
+        {
+            double mantissa = (Random.NextDouble() * 2.0) - 1.0;
+            // choose -149 instead of -126 to also generate subnormal floats (*)
+            double exponent = Math.Pow(2.0, Random.Next(-126, 128));
+            return (float)(mantissa * exponent);
+        }
+
+        static void Main(string[] args)
+        {
+            using var streamWriter = new StreamWriter("V5Data.txt");
+            for (int i = 0; i < 100; i++)
+            {
+                streamWriter.WriteLine($"{NextFloat()} {NextFloat()} {NextFloat()} {NextFloat()}");
+            }
+        }
+    }
+}
